23af558560adc200d9dbe790d4d204b4dbab1e77
Khensani16 <144327456+Khensani16@users.noreply.github.com>
2026-08-14 10:45:19 +0200

    First Commit

---
+++ TaskCloud/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskCloud
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a c# consle application");
+            Console.WriteLine("Running an NET 10.0 with c# 14.0 features");
+        }
+    }
+}
