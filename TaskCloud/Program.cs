2101aed2bc5c60547b46dea87b89c7602d8c3166
Khensani16 <144327456+Khensani16@users.noreply.github.com>
2026-08-14 11:10:02 +0200

    Third Commit

--- TaskCloud/Program.cs
+++ TaskCloud/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye bye world!");
             Console.WriteLine("This is a c# consle application");
             Console.WriteLine("Running an NET 10.0 with c# 14.0 features");
         }
